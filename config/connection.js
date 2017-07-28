0db589973bd29b02b95377cca93475a1d06dd7ae
School <school@Davids-MacBook-Pro.local>
2017-07-28 00:07:42 -0700

    JAWSDB for Heroku Deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Require mysql
 var mysql = require("mysql");
 
-// Set up MySQL connection. Use correct credentials
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// Set up MySQL connection. If deployed through Heroku, use JAWSDB
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql://tkgz7fzuukbdr4m7:neqyig6hi9a2gk6l@lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ezddphwlec9p1ie4
+}else {}
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
